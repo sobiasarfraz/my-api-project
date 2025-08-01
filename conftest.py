e72ccbfb98c76ae8023922a3d0dee99f73cb196e
sobiasarfraz <moon_202119@yahoo.com>
2025-07-31 23:34:27 -0400

    Updated conftest.py and fixed WebDriver setup

--- conftest.py
+++ conftest.py
@@ -4,9 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 import logging
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
@@ -20,8 +17,7 @@ def setup(request):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.maximize_window()
 
-    # Adding WebDriverWait to the driver for explicit waits
-    driver.wait = WebDriverWait(driver, 10)  # waits up to 10 seconds
+
 
     request.cls.driver = driver  # 🔥 makes self.driver work inside class
     yield
