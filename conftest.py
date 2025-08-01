f339fc20ea1078e9e362b283830a1f7d6ae641e9
sobiasarfraz <moon_202119@yahoo.com>
2025-07-31 23:18:48 -0400

    Tests passed and report generated

--- conftest.py
+++ conftest.py
@@ -7,10 +7,16 @@ import logging
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="class")
 def setup(request):
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.maximize_window()
 
